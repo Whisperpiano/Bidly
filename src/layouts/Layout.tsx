591e544a74e0d00dcaab2a65003b523f03994196
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-11-18 15:48:57 +0100

    primary color variables

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -15,22 +15,26 @@ function Layout() {
   };
 
   return (
-    <div className={darkMode ? "dark" : ""} id="layout">
-      <nav className="bg-gray-800 p-4">
+    <section className={darkMode ? "dark" : ""} id="layout">
+      <nav className="bg-primary-800 p-4">
         <Link to="/" className="text-white mx-2">
           Home
         </Link>
         <Link to="/about" className="text-white mx-2">
           About
         </Link>
-        <button className="text-white mx-2" onClick={toggleTheme}>
+        <button
+          className="text-white mx-2"
+          onClick={toggleTheme}
+          aria-label="Toggle dark mode"
+        >
           Toggle theme
         </button>
       </nav>
       <main className="p-4 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-white min-h-screen">
         <Outlet />
       </main>
-    </div>
+    </section>
   );
 }
 
