83822b3a7af8477fb71760b64e89c55860cff9ab
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-11-18 10:37:49 +0100

    add toggle dark mode

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,21 @@
 import { Link, Outlet } from "react-router-dom";
 import { useTestStore } from "../store/test";
+import { useState } from "react";
 
 function Layout() {
+  const [darkMode, setDarkMode] = useState(false);
+
   // no necesariamente aqui, porque es global ya
   // se puede acceder desde cualquier componente
   const test = useTestStore((state) => state.tests);
   console.log(test);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div>
+    <div className={darkMode ? "dark" : ""} id="layout">
       <nav className="bg-gray-800 p-4">
         <Link to="/" className="text-white mx-2">
           Home
@@ -15,8 +23,11 @@ function Layout() {
         <Link to="/about" className="text-white mx-2">
           About
         </Link>
+        <button className="text-white mx-2" onClick={toggleTheme}>
+          Toggle theme
+        </button>
       </nav>
-      <main className="p-4">
+      <main className="p-4 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-white min-h-screen">
         <Outlet />
       </main>
     </div>
