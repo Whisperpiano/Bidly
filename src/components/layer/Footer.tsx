af620aacb4616a0fb2299384ef21d396f31fb66b
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-11-19 09:19:31 +0100

    styled footer

--- src/components/layer/Footer.tsx
+++ src/components/layer/Footer.tsx
@@ -1,17 +1,29 @@
-import { useThemeStore } from "../../store/theme";
+import {
+  PiTwitterLogoFill,
+  PiInstagramLogoFill,
+  PiDiscordLogoFill,
+} from "react-icons/pi";
+import Button from "./Button";
+import Logo from "./Logo";
 
 export default function Footer() {
-  const toggleTheme = useThemeStore((state) => state.toggleTheme);
   return (
-    <footer className="bg-primary-800 p-4 text-white">
-      <p>This is the footer</p>
-      <button
-        className="text-white mx-2"
-        onClick={toggleTheme}
-        aria-label="Toggle dark mode"
-      >
-        Toggle theme
-      </button>
+    <footer className="max-w-screen-2xl mx-auto flex items-center justify-between w-full px-2.5 py-5 relative border-t dark:border-neutral-800 border-neutral-200">
+      <Logo />
+      <p className="dark:text-neutral-400 text-neutral-600 text-sm absolute left-1/2 transform -translate-x-1/2">
+        © 2024 Bidly Inc. All rights reserved
+      </p>
+      <div className="flex items-center gap-2">
+        <Button type="outlined" ariaLabel="Twitter">
+          <PiTwitterLogoFill size={20} />
+        </Button>
+        <Button type="outlined" ariaLabel="Instagram">
+          <PiInstagramLogoFill size={20} />
+        </Button>
+        <Button type="outlined" ariaLabel="Discord">
+          <PiDiscordLogoFill size={20} />
+        </Button>
+      </div>
     </footer>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import { useThemeStore } from "../store/theme";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const toggleTheme = useThemeStore((state) => state.toggleTheme);
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button
+        className="text-white mx-2"
+        onClick={toggleTheme}
+        aria-label="Toggle dark mode"
+      >
+        Toggle theme
+      </button>
+    </div>
+  );
 };
 
 export default Home;
