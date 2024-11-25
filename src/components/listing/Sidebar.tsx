89b59555e0a3da726f89fd3281f61b29fbb226b9
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-11-25 09:51:51 +0100

    light theme single item

--- src/components/listing/Sidebar.tsx
+++ src/components/listing/Sidebar.tsx
@@ -30,9 +30,9 @@ export default function Sidebar() {
           <Badge text={"rings"} />
           <Badge text={"accesory"} />
         </div>
-        <div className="grid grid-cols-2 gap-2 py-6 border-y dark:border-neutral-800 border-neutral-200">
-          <Link to={`/profile/username`}>
-            <div className="flex gap-3 items-center">
+        <div className="grid grid-cols-2 py-3 border-y dark:border-neutral-800 border-neutral-200">
+          <Link to={`/profile/username`} className="group">
+            <div className="flex gap-3 items-center py-3 px-2 rounded-lg dark:hover:bg-neutral-900 hover:bg-neutral-200/50">
               <img
                 src="https://images.unsplash.com/photo-1514207994142-98522b5a2b23?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="alt placeholder"
@@ -42,15 +42,15 @@ export default function Sidebar() {
                 <span className="text-xs dark:text-neutral-400 text-neutral-500 font-semibold">
                   Creator
                 </span>
-                <span className="text-sm dark:text-neutral-50 text-neutral-900 font-semibold flex gap-1 items-center">
+                <span className="text-sm dark:text-neutral-50 text-neutral-900 font-semibold flex gap-1 items-center group-hover:underline">
                   username
                   <PiSealCheckFill className="text-yellow-400 " />
                 </span>
               </div>
             </div>
           </Link>
-          <Link to={`/profile/username`}>
-            <div className="flex gap-3 items-center">
+          <Link to={`/profile/username`} className="group">
+            <div className="flex gap-3 items-center py-3 px-2 rounded-lg dark:hover:bg-neutral-900 hover:bg-neutral-200/50">
               <img
                 src="https://images.unsplash.com/photo-1514207994142-98522b5a2b23?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="alt placeholder"
@@ -60,7 +60,7 @@ export default function Sidebar() {
                 <span className="text-xs dark:text-neutral-400 text-neutral-500 font-semibold">
                   Winner
                 </span>
-                <span className="text-sm dark:text-neutral-50 text-neutral-900 font-semibold flex gap-1 items-center">
+                <span className="text-sm dark:text-neutral-50 text-neutral-900 font-semibold flex gap-1 items-center group-hover:underline">
                   username
                   <PiSealCheckFill className="text-yellow-400 " />
                 </span>
@@ -70,28 +70,28 @@ export default function Sidebar() {
         </div>
         <div className="flex items-center justify-between py-6">
           <div className="flex gap-4 items-center">
-            <button className="flex gap-1 items-center text-sm dark:text-neutral-400 text-neutral-500 font-medium">
+            <button className="flex gap-1 items-center text-sm dark:text-neutral-400 dark:hover:text-neutral-200 text-neutral-500 hover:text-neutral-700 font-medium">
               <PiShareFatFill />
               Share
             </button>
-            <button className="flex gap-1 items-center text-sm dark:text-neutral-400 text-neutral-500 font-medium">
+            <button className="flex gap-1 items-center text-sm dark:text-neutral-400 dark:hover:text-neutral-200 text-neutral-500 hover:text-neutral-700 font-medium">
               <PiArrowClockwiseBold />
               Refresh
             </button>
           </div>
-          <button className="dark:text-neutral-400 text-neutral-500">
+          <button className="dark:text-neutral-400 dark:hover:text-neutral-200 text-neutral-500 hover:text-neutral-700">
             <span className="sr-only">Options</span>
             <PiDotsThreeBold size={20} />
           </button>
         </div>
         <div className="grid grid-cols-2 gap-2 text-xs font-semibold">
-          <div className="flex flex-col gap-1.5 border rounded-lg p-5 dark:bg-neutral-900 bg-neutral-100 dark:border-neutral-800 border-neutral-100">
+          <div className="flex flex-col gap-1.5 border rounded-lg p-5 dark:bg-neutral-900 bg-neutral-100 dark:border-neutral-800 border-neutral-200/50">
             <span className="dark:text-neutral-400 text-neutral-600">
               Price
             </span>
             <span>100 NOFF</span>
           </div>
-          <div className="flex flex-col gap-1.5 border rounded-lg p-5 dark:bg-neutral-900 bg-neutral-100 dark:border-neutral-800 border-neutral-100">
+          <div className="flex flex-col gap-1.5 border rounded-lg p-5 dark:bg-neutral-900 bg-neutral-100 dark:border-neutral-800 border-neutral-200/50">
             <span className="dark:text-neutral-400 text-neutral-600">
               Sale ends
             </span>
@@ -112,9 +112,9 @@ export default function Sidebar() {
           >
             Place a bid
           </Button>
-          <Button type="secondary" ariaLabel="Add to cart">
+          {/* <Button type="secondary" ariaLabel="Add to cart">
             Update
-          </Button>
+          </Button> */}
         </div>
       </article>
 
--- src/components/modal/PlaceBidModal.tsx
+++ src/components/modal/PlaceBidModal.tsx
@@ -30,19 +30,19 @@ export default function PlaceBidModal({ isOpen, onClose }: ModalProps) {
       onClick={onClose}
     >
       <article
-        className="relative p-6 flex overflow-hidden max-w-sm bg-neutral-900 rounded-lg w-full"
+        className="relative p-6 flex overflow-hidden max-w-sm dark:bg-neutral-900 bg-neutral-50 rounded-lg w-full"
         onClick={(e) => e.stopPropagation()}
       >
         <form onClick={test} className="w-full">
           <button
             type="button"
             onClick={onClose}
-            className="absolute z-50 top-3 right-3 rounded-full p-2 dark:bg-neutral-800 dark-text-neutral-50 hover:bg-neutral-700"
+            className="absolute z-50 top-3 right-3 rounded-full p-2 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-700 bg-neutral-200/50 text-neutral-900 hover:bg-neutral-200"
           >
             <span className="sr-only">Close auth modal</span>
             <PiXBold size={15} />
           </button>
-          <h2 className="pb-6 text-lg font-medium dark:text-neutral-50">
+          <h2 className="pb-3 text-lg font-medium dark:text-neutral-50 text-neutral-900">
             Place a bid
           </h2>
           <p className="pb-3 text-sm dark:text-neutral-50 text-neutral-900 ">
@@ -52,7 +52,7 @@ export default function PlaceBidModal({ isOpen, onClose }: ModalProps) {
             <label htmlFor="bid" className="sr-only">
               Place your bid for this item
             </label>
-            <span className="absolute top-3 right-3 text-xs uppercase dark:text-neutral-50 font-normal">
+            <span className="absolute top-3 right-3 text-xs uppercase dark:text-neutral-50 text-neutral-900 font-normal">
               NOFF
             </span>
             <input
@@ -60,45 +60,53 @@ export default function PlaceBidModal({ isOpen, onClose }: ModalProps) {
               name="bid"
               type="text"
               placeholder="Enter your NOFF-tastic offer here!"
-              className="pe-12 w-full p-2.5 rounded-lg border outline-none text-sm placeholder:text-xs dark:bg-neutral-800 dark:border-neutral-800 dark:text-neutral-50 dark:focus:border-neutral-500 dark:placeholder:text-neutral-400"
+              className="pe-12 w-full p-2.5 rounded-lg border outline-none text-sm placeholder:text-xs dark:bg-neutral-800 dark:border-neutral-800 dark:text-neutral-50 dark:focus:border-neutral-500 dark:placeholder:text-neutral-400 bg-neutral-200/50 text-neutral-900 focus:border-neutral-400/50 placeholder:text-neutral-500 focus:bg-neutral-50"
               required
             />
           </div>
 
-          <div className="border rounded-lg p-3 text-xs flex flex-col gap-3 dark:border-neutral-800 border-neutral-100">
+          <div className="border rounded-lg p-3 text-xs flex flex-col gap-3 dark:border-neutral-800 border-neutral-200">
             <div className="flex items-center justify-between">
-              <span className="font-semibold dark:text-neutral-400">Price</span>
-              <span className="dark:text-neutral-50">150 NOFF</span>
+              <span className="font-semibold dark:text-neutral-400 text-neutral-500">
+                Price
+              </span>
+              <span className="dark:text-neutral-50 text-neutral-900">
+                150 NOFF
+              </span>
             </div>
             <div className="flex items-center justify-between">
-              <span className="font-semibold dark:text-neutral-400">
+              <span className="font-semibold dark:text-neutral-400 text-neutral-500">
                 Your coins
               </span>
-              <span className="dark:text-green-500">1500 NOFF</span>
+              <span className="dark:text-green-500 text-green-700">
+                1500 NOFF
+              </span>
             </div>
             <div className="flex items-center justify-between">
-              <span className="font-semibold dark:text-neutral-400">
+              <span className="font-semibold dark:text-neutral-400 text-neutral-500">
                 You pay
               </span>
-              <span className="dark:text-red-400">-50 NOFF</span>
+              <span className="dark:text-red-400 text-red-600">-50 NOFF</span>
             </div>
-            <div className="h-0.5 bg-neutral-100 dark:bg-neutral-800"></div>
+            <div className="h-0.5 bg-neutral-200 dark:bg-neutral-800"></div>
             <div className="flex items-center justify-between">
-              <span className="font-semibold dark:text-neutral-400">
+              <span className="font-semibold dark:text-neutral-400 text-neutral-500">
                 Your balance
               </span>
-              <span className="font-bold dark:text-neutral-50">980 NOFF</span>
+              <span className="font-bold dark:text-neutral-50 text-neutral-900">
+                980 NOFF
+              </span>
             </div>
           </div>
 
-          <span className=" my-3 text-xs dark:text-red-400 flex justify-center">
+          <span className=" my-3 text-xs dark:text-red-400 text-red-600 flex justify-center">
             Not enough coins!
           </span>
 
           <button
             type="submit"
             onClick={test}
-            className="w-full p-2.5 rounded-lg text-sm font-semibold dark:bg-primary-600 dark:text-neutral-50 dark:hover:bg-primary-700"
+            className="w-full p-2.5 rounded-lg text-sm font-medium dark:bg-primary-600 dark:text-neutral-50 dark:hover:bg-primary-700 bg-primary-600 text-neutral-50 hover:bg-primary-700"
           >
             Confirm bid
           </button>
