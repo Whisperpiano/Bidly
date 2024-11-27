a01b15d2af2a4d34f4515b7a6d21534b11367102
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-11-27 11:45:46 +0100

    styles hamburguer menu

--- src/components/layer/HamburguerMenu.tsx
+++ src/components/layer/HamburguerMenu.tsx
@@ -66,17 +66,17 @@ export default function HamburguerMenu() {
       </button>
       <section>
         <div
-          className={`fixed top-[87px] left-0 w-full h-screen bg-neutral-950/25 transition-all duration-800 overflow-hidden ${
+          className={`fixed top-[87px] sm:top-[96px] left-0 w-full h-screen bg-neutral-950/25  backdrop-blur-sm transition-all duration-800 overflow-hidden ${
             isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
           }`}
         >
           <div
-            className={`flex flex-col bg-neutral-50 dark:bg-neutral-950 h-screen max-w-[300px] ml-auto transition-all duration-800 ${
-              isOpen ? "translate-x-10" : "translate-x-[200px]"
+            className={`flex flex-col bg-neutral-50  dark:bg-neutral-900 h-screen max-w-[300px] ml-auto transition-all duration-800 ${
+              isOpen ? "translate-x-0" : "translate-x-[200px]"
             }`}
             ref={menuRef}
           >
-            <div className="p-3 flex items-center gap-3 select-none">
+            <div className="px-3 py-6 border-b border-neutral-200 dark:border-neutral-800 flex items-center gap-3 select-none">
               <div>
                 <img
                   src="https://images.unsplash.com/photo-1514207994142-98522b5a2b23?q=80&w=1526&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -94,9 +94,9 @@ export default function HamburguerMenu() {
               </div>
             </div>
 
-            <ul className="py-2 text-sm select-none">
+            <ul className="py-2 border-b border-neutral-200 dark:border-neutral-800 text-sm select-none">
               <li>
-                <span className="px-4 py-2 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
+                <span className="p-3 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
                   <PiUserFill
                     size={16}
                     className="dark:text-neutral-300 text-neutral-700 duration-0"
@@ -105,7 +105,7 @@ export default function HamburguerMenu() {
                 </span>
               </li>
               <li>
-                <span className="px-4 py-2 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
+                <span className="p-3 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
                   <PiTagFill
                     size={16}
                     className="dark:text-neutral-300 text-neutral-700 duration-0"
@@ -114,10 +114,10 @@ export default function HamburguerMenu() {
                 </span>
               </li>
             </ul>
-            <div className="py-2 text-sm ">
+            <div className="py-2 text-sm border-b border-neutral-200 dark:border-neutral-800">
               <ul className="select-none">
                 <li>
-                  <span className="px-4 py-2 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
+                  <span className="p-3 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
                     <PiGearFill
                       size={16}
                       className="dark:text-neutral-300 text-neutral-700 duration-0"
@@ -126,7 +126,7 @@ export default function HamburguerMenu() {
                   </span>
                 </li>
                 <li>
-                  <span className="px-4 py-2 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
+                  <span className="p-3 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 text-neutral-900 dark:text-neutral-300 hover:bg-neutral-200/50 hover:text-neutral-900 flex items-center gap-1.5">
                     <PiGlobeFill
                       size={16}
                       className="dark:text-neutral-300 text-neutral-700 duration-0"
@@ -135,7 +135,7 @@ export default function HamburguerMenu() {
                   </span>
                 </li>
                 <li>
-                  <label className="inline-flex w-full items-center justify-between cursor-pointer px-4 py-2 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 hover:bg-neutral-200/50 hover:text-neutral-900">
+                  <label className="inline-flex w-full items-center justify-between cursor-pointer p-3 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 hover:bg-neutral-200/50 hover:text-neutral-900">
                     <input
                       type="checkbox"
                       className="sr-only peer"
@@ -157,7 +157,7 @@ export default function HamburguerMenu() {
               </ul>
             </div>
             <div className="py-2 dark:text-red-400 text-red-600 select-none duration-0">
-              <span className="px-4 py-2 text-sm dark:hover:bg-neutral-800 hover:bg-neutral-200/50 flex items-center gap-1.5 ">
+              <span className="p-3 text-sm dark:hover:bg-neutral-800 hover:bg-neutral-200/50 flex items-center gap-1.5 ">
                 <PiSignOutFill
                   size={16}
                   className="dark:text-red-400  text-red-600 duration-0"
