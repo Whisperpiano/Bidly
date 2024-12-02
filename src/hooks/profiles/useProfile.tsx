a396ca383679072da559a463177051097056b828
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-12-02 12:32:17 +0100

    refactor profile and added custom hook to get profile

--- src/hooks/profiles/useProfile.tsx
+++ src/hooks/profiles/useProfile.tsx
@@ -1,1 +1,53 @@
-export function useProfile() {}
+import { useEffect, useState } from "react";
+import { UserProfile } from "../../types/types";
+import { useParams } from "react-router-dom";
+import getSingleProfile from "../../api/profiles/getSingleProfile";
+
+export function useProfile() {
+  const [profile, setProfile] = useState<UserProfile | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const { username } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchProfile() {
+      try {
+        if (!username) {
+          return;
+        }
+        const profile = await getSingleProfile({ username });
+
+        if (!profile) {
+          setIsError(true);
+          setErrorMessage("Invalid profile");
+          return;
+        }
+
+        if ("data" in profile) {
+          setIsError(false);
+          setErrorMessage("");
+          setProfile(profile.data);
+        } else if ("errors" in profile) {
+          setIsError(true);
+          setErrorMessage(profile?.errors?.[0]?.message || "Unknown error");
+          setProfile(null);
+        }
+      } catch (error) {
+        setIsError(true);
+        setErrorMessage(` Something went wrong fetching the profile: ${error}`);
+        setProfile(null);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchProfile();
+  }, [username]);
+  return {
+    profile,
+    isLoading,
+    isError,
+    errorMessage,
+  };
+}
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,58 +1,16 @@
-import { useParams } from "react-router-dom";
 import FilterAndSort from "../components/profile/FilterAndSort";
 import ProfileBanner from "../components/profile/ProfileBanner";
 import ProfileNavigationHeader from "../components/profile/ProfileNavigationHeader";
 import { useEffect, useState } from "react";
-import { Listing, UserProfile } from "../types/types";
-import getSingleProfile from "../api/profiles/getSingleProfile";
+import { Listing } from "../types/types";
 import ProfileItemCard from "../components/grid/ProfileItemCard";
 import { sortListings } from "../utils/sortListings";
+import { useProfile } from "../hooks/profiles/useProfile";
 
 export default function Profile() {
-  const [profile, setProfile] = useState<UserProfile | null>(null);
+  const { profile } = useProfile();
   const [listings, setListings] = useState<Listing[]>([]);
   const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
-
-  const { username } = useParams();
-
-  useEffect(() => {
-    setIsLoading(true);
-    async function fetchProfile() {
-      try {
-        if (!username) {
-          setIsError(true);
-          setErrorMessage("Invalid username provided");
-          return;
-        }
-        const profile = await getSingleProfile({ username });
-
-        if (!profile) {
-          setIsError(true);
-          setErrorMessage("Invalid profile");
-          return;
-        }
-
-        if ("data" in profile) {
-          setIsError(false);
-          setErrorMessage("");
-          setProfile(profile.data);
-        } else if ("errors" in profile) {
-          setIsError(true);
-          setErrorMessage(profile?.errors?.[0]?.message || "Unknown error");
-        }
-      } catch (error) {
-        setIsError(true);
-        setErrorMessage(` Something went wrong fetching the profile: ${error}`);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-
-    fetchProfile();
-  }, [username]);
 
   useEffect(() => {
     if (!profile) return;
@@ -61,14 +19,10 @@ export default function Profile() {
     setListings(sortedListings);
   }, [profile, selectedFilter]);
 
-  console.log(errorMessage);
-  console.log(isLoading);
-  console.log(isError);
-
   // TODO hacer mejor el loading
   return (
     <>
-      <section className="mt-6">
+      <section className=" mt-6">
         {!profile ? <div>Loading...</div> : <ProfileBanner profile={profile} />}
         <div className="px-2 md:px-4 lg:px-6 -mt-8 sm:-mt-12  md:-mt-16 lg:-mt-20 ">
           <ProfileNavigationHeader />
@@ -76,12 +30,19 @@ export default function Profile() {
             filter={selectedFilter}
             setFilter={setSelectedFilter}
           />
-
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 py-6 px-0 md:px-2">
-            {listings.map((listing) => (
-              <ProfileItemCard key={listing.id} item={listing} />
-            ))}
-          </div>
+          {listings.length === 0 ? (
+            <div className="w-full border dark:border-neutral-800 border-neutral-200 rounded-lg my-6 min-h-[650px] flex items-center justify-center">
+              <span className="text-base md:text-lg dark:text-neutral-400 text-neutral-500">
+                No listings created yet.
+              </span>
+            </div>
+          ) : (
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 py-6 px-0 md:px-2 min-h-[650px]">
+              {listings.map((listing) => (
+                <ProfileItemCard key={listing.id} item={listing} />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
