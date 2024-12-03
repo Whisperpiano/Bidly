1bd90c168f33e3a4599be62e4f5a0a466f02c39d
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-12-03 09:46:36 +0100

    try to improve a bit more with loadings

--- src/components/profile/ProfileBanner.tsx
+++ src/components/profile/ProfileBanner.tsx
@@ -6,43 +6,17 @@ import { UserProfile } from "../../types/types";
 import { useAuthStore } from "../../store/user";
 import { useEffect, useState } from "react";
 import { useImgur } from "../../hooks/imgur/useImgur";
-// import { uploadPicture } from "../../api/imgur/uploadPicture";
-// import updateProfile from "../../api/profiles/updateProfile";
+import Spinner from "../elements/Spinner";
 
 export default function ProfileBanner({ profile }: { profile: UserProfile }) {
   const userName = useAuthStore((state) => state.username);
   const [avatar, setAvatar] = useState<string>("");
   const [banner, setBanner] = useState<string>("");
-  const { handleFileChangeAndUpload, isUploading, noroffApiError, imgurError } =
-    useImgur();
-
-  // const handleAvatarChange = async (
-  //   event: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   const file = event.target.files?.[0];
-
-  //   if (file) {
-  //     const picture = await uploadPicture(file);
-  //     await updateProfile({ avatar: picture });
-  //     console.log(picture);
-  //     setAvatar(picture);
-  //   }
-  // };
-
-  // const handleBannerChange = async (
-  //   event: React.ChangeEvent<HTMLInputElement>
-  // ) => {
-  //   const file = event.target.files?.[0];
-
-  //   if (file) {
-  //     const picture = await uploadPicture(file);
-  //     await updateProfile({ banner: picture });
-  //     console.log(picture);
-  //     setBanner(picture);
-  //   }
-  // };
-
-  console.log(isUploading, noroffApiError, imgurError);
+  const {
+    handleFileChangeAndUpload,
+    isUploading: avatarUploading,
+    isUploading: bannerUploading,
+  } = useImgur();
 
   useEffect(() => {
     setAvatar(profile.avatar.url ?? "https://placehold.co/260x160");
@@ -55,19 +29,33 @@ export default function ProfileBanner({ profile }: { profile: UserProfile }) {
         <img
           src={banner}
           alt={`Banner of ${profile.name}`}
-          className="h-[200px] md:h-[220px] lg:h-[300px] w-full object-cover object-center rounded-lg"
+          className={`h-[200px] md:h-[220px] lg:h-[300px] w-full object-cover object-center rounded-lg transition-opacity duration-300 ${
+            bannerUploading ? "opacity-50" : ""
+          }`}
         />
+        {bannerUploading && (
+          <div className="absolute inset-0 z-[5] flex items-center justify-center animate-reveal">
+            <Spinner />
+          </div>
+        )}
         <ProfileStats count={profile._count} credits={profile.credits} />
       </div>
 
       {/* Profile */}
       <div className="flex justify-between p-2 md:p-4 lg:p-6 -translate-y-1/2">
         <div className="flex items-end gap-2 md:gap-3 lg:gap-4">
-          <img
-            src={avatar}
-            alt={`Avatar of ${profile.name}`}
-            className="aspect-square w-20 sm:w-24 md:w-28 object-cover object-center rounded-lg border dar:border-neutral-950 dark:border-neutral-950 border-neutral-50"
-          />
+          <div className="relative">
+            <img
+              src={avatar}
+              alt={`Avatar of ${profile.name}`}
+              className={`aspect-square w-20 sm:w-24 md:w-28 object-cover object-center rounded-lg border dar:border-neutral-950 dark:border-neutral-950 border-neutral-50`}
+            />
+            {avatarUploading && (
+              <div className="absolute inset-0 z-[5] flex items-center justify-center animate-reveal bg-neutral-900/50">
+                <Spinner />
+              </div>
+            )}
+          </div>
           <div className="flex gap-1 items-center mb-2">
             <h1 className="text-base md:text-xl font-semibold dark:text-neutral-50 text-neutral-900">
               {profile.name}
