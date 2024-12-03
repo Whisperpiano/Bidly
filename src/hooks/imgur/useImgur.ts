96ae352d35d21757642cb989682f78d87fc17bd6
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-12-03 09:28:36 +0100

    try to improve upload pic

--- src/hooks/imgur/useImgur.ts
+++ src/hooks/imgur/useImgur.ts
@@ -1,11 +1,16 @@
 import { useState } from "react";
 import { uploadPicture } from "../../api/imgur/uploadPicture";
 import updateProfile from "../../api/profiles/updateProfile";
+import { useAuthStore } from "../../store/user";
 
 export function useImgur() {
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [imgurError, setImgurError] = useState<string | null>(null);
   const [noroffApiError, setNoroffApiError] = useState<string | null>(null);
+
+  const setAuth = useAuthStore((state) => state.setAuth);
+  const username = useAuthStore((state) => state.username);
+  const accessToken = useAuthStore((state) => state.accessToken);
 
   const handleFileChangeAndUpload = async (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -37,6 +42,10 @@ export function useImgur() {
       } else if ("errors" in update) {
         setNoroffApiError(update.errors[0].message);
         throw new Error(update.errors[0].message);
+      }
+
+      if (username && accessToken) {
+        setAuth(username, accessToken);
       }
     } catch (error) {
       console.log(`Uknown error uploading image: ${error}`);
