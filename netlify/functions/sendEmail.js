0235082fcbba59f90dde963b23e38afc71978e6d
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-12-09 10:10:29 +0100

    more

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -10,7 +10,7 @@ export async function handler(event) {
       from: "Acme <onboarding@resend.dev>",
       to: body.to || ["default@example.com"],
       subject: body.subject || "Hello World!",
-      html: body.react || "<strong>It worksss!</strong>",
+      html: body.html || "<strong>It worksss!</strong>",
     });
 
     return {
