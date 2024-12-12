f5c1579a7f388380872648fddd4611d7ae4a2a5c
Jesús Alberola Herrero <jesusalberola90@gmail.com>
2024-12-12 11:48:03 +0100

    fixed gemini

--- src/api/gemini/generateText.ts
+++ src/api/gemini/generateText.ts
@@ -2,10 +2,10 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { toast } from "sonner";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_AI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 export default async function generateAIText(prompt: string) {
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const result = await model.generateContent(prompt);
     return result.response.text();
   } catch (error) {
